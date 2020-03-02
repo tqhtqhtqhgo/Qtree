dc44ad92652ef04deece40e4e093894cb88ec560
caffrey <sc18qt@gmail.com>
2020-03-02 13:53:51 +0800

    Ex4 and 5

---
+++ cmake-build-debug/CMakeFiles/Qtree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Qtree.dir/main.c.o"
+  "Qtree"
+  "Qtree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Qtree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
