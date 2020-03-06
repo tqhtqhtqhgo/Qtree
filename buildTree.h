5dc0db81ef2c7c0bdde72e4128e8b1b9ffa49bf9
caffrey <sc18qt@gmail.com>
2020-03-06 21:24:50 +0800

    update

--- buildTree.h
+++
@@ -1,8 +0,0 @@
-#include "treeStructure.h"
-
-
-Node *makeNode( double x, double y, int level );
-void makeChildren( Node *parent );
-void destroyTree();
-int growTree();
-int preVisitQuadTree();
--- writeTree.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by caaffrey on 2020/3/6.
-//
-#include "buildTree.h"
-
-void writeTree( Node *head );
-void writeNode( FILE *fp, Node *node );
-void printOut( FILE *fp, Node *node );
